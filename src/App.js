807dbbe5caec0206f299337394f74543e53d1f39
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2021-03-15 15:58:11 -0700

    mvp done

--- src/App.js
+++ src/App.js
@@ -1,62 +1,108 @@
-import React from 'react';
-
-import './App.css';
-
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import React, { useReducer } from "react";
+import reducer, { initialState } from "./reducers";
+import "./App.css";
+import {
+  addOne,
+  applyNumber,
+  changeOperation,
+  clearDisplay,
+  mClear,
+  mPlus,
+  mResult,
+} from "./actions";
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // const eventHandlerOne = () => {
+  //   dispatch(addOne());
+  // };
+
+  const eventHandler = (num) => {
+    dispatch(applyNumber(num));
+  };
+
+  const operatorChanger = (operator) => {
+    dispatch(changeOperation(operator));
+  };
+
+  const clear_display = () => {
+    dispatch(clearDisplay());
+  };
+
+  const m_plus = () => {
+    dispatch(mPlus());
+  };
+
+  const m_result = () => {
+    dispatch(mResult());
+  };
+
+  const m_clear = () => {
+    dispatch(mClear());
+  };
 
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
+        <a className="navbar-brand" href="#">
+          <img width="40px" src="./Lambda-Logo-Red.png" /> Lambda Reducer
+          Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
+      <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={"M+"} onClick={() => m_plus()} />
+              <CalcButton value={"MR"} onClick={() => m_result()} />
+              <CalcButton value={"MC"} onClick={() => m_clear()} />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton
+                value={1}
+                //onClick={() => eventHandlerOne()}
+                onClick={() => eventHandler(1)}
+              />
+              <CalcButton value={2} onClick={() => eventHandler(2)} />
+              <CalcButton value={3} onClick={() => eventHandler(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={4} onClick={() => eventHandler(4)} />
+              <CalcButton value={5} onClick={() => eventHandler(5)} />
+              <CalcButton value={6} onClick={() => eventHandler(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={7} onClick={() => eventHandler(7)} />
+              <CalcButton value={8} onClick={() => eventHandler(8)} />
+              <CalcButton value={9} onClick={() => eventHandler(9)} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={"+"} onClick={() => operatorChanger("+")} />
+              <CalcButton value={"*"} onClick={() => operatorChanger("*")} />
+              <CalcButton value={"-"} onClick={() => operatorChanger("-")} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={() => clear_display()} />
             </div>
-
           </form>
         </div>
       </div>
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,35 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const M_PLUS = "M_PLUS";
+export const M_RESULT = "M_RESULT";
+export const M_CLEAR = "M_CLEAR";
 
 export const addOne = () => {
-    return({type:ADD_ONE});
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const changeOperation = (operator) => {
+  return { type: CHANGE_OPERATION, payload: operator };
+};
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY };
+};
+
+export const mPlus = () => {
+  return { type: M_PLUS };
+};
+
+export const mResult = () => {
+  return { type: M_RESULT };
+};
+
+export const mClear = () => {
+  return { type: M_CLEAR };
+};
